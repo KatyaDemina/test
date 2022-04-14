5405a062bfff564856b845d7d42edb729b2578a9
KatyaDemina <demina-ekaterina-88@mail.ru>
2022-04-14 20:09:24 +0400

    My program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Привет, это моя программа на С#!");
+        }
+    }
+}
